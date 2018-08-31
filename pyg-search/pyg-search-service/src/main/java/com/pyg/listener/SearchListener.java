7df071e93458c1581b164daf7b338be838301810
frank email=ityyfrank@gmail.com <ityyfrank@gmail.com>
2018-08-31 09:18:00 +0800

    this is 3st commit

---
+++ pyg-search/pyg-search-service/src/main/java/com/pyg/listener/SearchListener.java
@@ -0,0 +1,59 @@
+package com.pyg.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.pyg.pojo.TbItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrTemplate;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 接收消息，实现索引库同步
+ * 业务流程：
+ * 1，接收消息
+ * 2，把消息转换为对象
+ * 3，使用solr模板把数据添加索引库即可实现索引库同步
+ */
+public class SearchListener implements MessageListener{
+
+    //注入solr模板对象
+    @Autowired
+    private SolrTemplate solrTemplate;
+
+    @Override
+    public void onMessage(Message message) {
+        if(message instanceof TextMessage){
+            try {
+                TextMessage m = (TextMessage) message;
+                //获取消息
+                String itemJson = m.getText();
+
+                //转换为List集合
+                List<TbItem> itemList = JSON.parseArray(itemJson, TbItem.class);
+
+                //循环集合设置规格值
+                for (TbItem tbItem : itemList) {
+                    //获取商品规格值
+                    String spec = tbItem.getSpec();
+                    //转换成map
+                    Map<String,String> specMap = (Map<String,String>) JSON.parse(spec);
+                    //设置商品对象规格map
+                    tbItem.setSpecMap(specMap);
+                }
+
+                //把集合保存到索引库即可
+                solrTemplate.saveBeans(itemList);
+                solrTemplate.commit();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ pyg-search/pyg-search-service/src/test/java/com/pyg/mq/QueueConsumer.java
@@ -0,0 +1,62 @@
+package com.pyg.mq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
+
+import javax.jms.*;
+
+public class QueueConsumer {
+
+
+    /**
+     * 需求：发送消息
+     * 模式：点对点模式
+     */
+    @Test
+    public void receiveMessage() throws Exception {
+        //指定消息服务器发送地址
+        String brokerURL = "tcp://193.112.23.174:61616";
+        //创建消息连接工厂对象
+        ConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
+        //从工厂获取连接对象
+        Connection connection = cf.createConnection();
+
+        //开启连接
+        connection.start();
+
+        //从连接中获取消息回话对象session
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        //创建消息存储空间，且给这个空间起一个名称
+        Queue queue = session.createQueue("myQueue");
+
+        //指定消息接收者，且指定从哪儿接受消息
+        MessageConsumer consumer = session.createConsumer(queue);
+
+        //监听接收
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+
+                if(message instanceof TextMessage){
+                    try {
+                        TextMessage m = (TextMessage) message;
+                        //获取消息
+                        String text = m.getText();
+                        System.out.println("接收消息："+text);
+                    } catch (JMSException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        //等待输入，让端口阻塞，服务一直开启
+        System.in.read();
+
+        consumer.close();
+        session.close();
+        connection.close();
+
+    }
+
+}
