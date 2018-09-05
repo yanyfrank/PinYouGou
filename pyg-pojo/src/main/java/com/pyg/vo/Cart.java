28506edfa9848cf0d737d93a64b766f26944cc14
frank email=ityyfrank@gmail.com <ityyfrank@gmail.com>
2018-09-05 18:31:07 +0800

    this is 4st commit

---
+++ pyg-pojo/src/main/java/com/pyg/vo/Cart.java
@@ -0,0 +1,42 @@
+package com.pyg.vo;
+
+import com.pyg.pojo.TbOrderItem;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 商家对象
+ */
+public class Cart implements Serializable {
+    //商家id
+    private String sellerId;
+    //商家名称
+    private String sellerName;
+    //商家购物列表
+    private List<TbOrderItem> orderItemList;
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public List<TbOrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List<TbOrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+}
